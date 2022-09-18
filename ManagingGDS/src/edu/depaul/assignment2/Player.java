d8db3e68f9dff30250fa2747d689a9e8e35cb526
Tatiana Donaldson <Tatiana_Donaldson@bcbsil.com>
2022-09-18 18:23:05 -0500

    Add Player java file to Assignment2 repository

---
+++ ManagingGDS/src/edu/depaul/assignment2/Player.java
@@ -0,0 +1,14 @@
+package edu.depaul.assignment2;
+
+public class Player {
+
+	public static int numPlayers;
+
+	public static void main(String[] args) {
+		Player player = new Player();
+		player.numPlayers=0;
+		System.out.println("Player object created!");
+	}
+
+	
+}
